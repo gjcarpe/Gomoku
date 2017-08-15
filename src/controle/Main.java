b534fcfa9913a6d7c3ff632ec8eb34d3e33c77f8
G. J. Carpeggiani <gustavocarpeggiani@hotmail.com>
2017-08-15 00:11:10 -0300

    Teste2

--- src/controle/Main.java
+++ src/controle/Main.java
@@ -1,9 +1,13 @@
 package controle;
 
+import view.Janela;
+
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Teste");
+		System.out.println("Iniciar...");
+		@SuppressWarnings("unused")
+		Janela janela = new Janela();
 	}
 }
---
+++ src/view/Janela.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class Janela extends JFrame
+{
+	public Janela()
+	{
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+}
