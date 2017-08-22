7cc978ff1daec77f3ce3d9ca927b2775c16c6bbc
G. J. Carpeggiani <gustavocarpeggiani@hotmail.com>
2017-08-22 15:40:02 -0300

    Updated README

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Gomoku
+
 UFSC - INE5430 
 
-Java implementation of the Gomoku game with Artificial Intelligence
+Java implementation of the Gomoku game with Artificial Intelligence player.
+
+Implemented by:
+
+@gjcarpe
+@JuanCuttle
--- src/controle/Main.java
+++ src/controle/Main.java
@@ -1,14 +1,24 @@
+/*
+ *  UFSC - Universidade Federal de Santa Catarina
+ *  Alunos: Gustavo José Carpeggiani
+ * 			Juan Alejandro Terenzi Cuttle
+ * 
+ *  Inteligência Artificial - Trabalho I - Gomoku
+ *  
+ *  GIT: https://github.com/gjcarpe/Gomoku
+ */
+
 package controle;
 
-import testes.Teste;
+//import testes.Teste;
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Iniciar...");
-		//Controle controle = new Controle();
-		//controle.iniciarJanela();
-		Teste teste = new Teste();
+		Controle controle = new Controle();
+		controle.iniciarJanela();
+		//Teste teste = new Teste();
 	}
 }
